a9dc94937ea2fa5b556c887ac17db2a90f8c35e8
Greg Woodcock <greg@yoomee.com>
2016-05-13 12:25:10 +0100

    Add heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :maze_game, MazeGame.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :maze_game, MazeGame.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
